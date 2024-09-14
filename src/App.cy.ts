72120b37bb2d3becb647b7b4c8ce245b3aff536f
@zhengpeixin <zhengpeixin@bingosoft.net>
2024-09-14 10:42:26 +0800

    init

---
+++ src/App.cy.ts
@@ -0,0 +1,5 @@
+import App from './App.vue';
+
+it('shiki', () => {
+  cy.mount(App);
+});
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>1</div>
+</template>
+
+<script setup lang="ts">
+import { codeToHtml } from 'shiki'
+console.log('codeToHtml: ', codeToHtml);
+</script>
